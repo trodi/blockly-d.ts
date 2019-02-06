d20866525f8ed3ef6be6274f2831e2a495a5ebc5
Emil Hemdal <emil@hemdal.se>
2019-02-06 09:18:01 +0100

    Moved asking for hash to the top

--- npm-scripts/generate.sh
+++ npm-scripts/generate.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Allow user to pick what commit hash they want to checkout
+echo Enter a commit hash or enter nothing to use the latest version of blockly
+read githash
+
 # Clear tmp directory if it exist
 rm -rf tmp
 
@@ -10,9 +14,6 @@ rm -rf dist
 mkdir tmp
 cd tmp
 git clone https://github.com/google/blockly.git
-
-echo Enter a commit hash or enter nothing to use the latest version of blockly
-read githash
 
 if [ -n $githash ]
 then
